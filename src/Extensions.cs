c6cf07fdb4bc6ec2b326d99e09b5839284a09273
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-11-17 22:04:41 +0100

    Added Array Fill extention

---
+++ src/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nuernberger
+{
+    public static class Extensions
+    {
+        public static T[] Populate<T>(this T[] arr, T value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = value;
+            }
+
+            return arr;
+        }
+    }
+}
